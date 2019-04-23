8205d3ea9aa1516403d8dd1c12dac361f95e37d2
7719668954 <49202086+7719668954@users.noreply.github.com>
2019-04-23 10:32:14 -0500

    Create mcdonalds_menu.java

---
+++ main/java/example/com/ezdine/mcdonalds_menu.java
@@ -0,0 +1,26 @@
+package example.com.ezdine;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class mcdonalds_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mcdonalds_menu);
+    }
+
+    public void openHome() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openChipotle() {
+        Intent intent = new Intent(this, mcdonalds_page.class);
+        startActivity(intent);
+    }
+}
