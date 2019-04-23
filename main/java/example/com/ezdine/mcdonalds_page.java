9a1aa178f44aaa601b7c9fac6f2eafbfd479adff
7719668954 <49202086+7719668954@users.noreply.github.com>
2019-04-23 10:37:00 -0500

    Update mcdonalds_page.java

--- main/java/example/com/ezdine/mcdonalds_page.java
+++ main/java/example/com/ezdine/mcdonalds_page.java
@@ -6,31 +6,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class chipotle_page extends AppCompatActivity {
+public class mcdonalds_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chipotle_page);
+        setContentView(R.layout.activity_mcdonalds_page);
 
-        Button butt6 = findViewById(R.id.button6);
-        butt6.setOnClickListener(new View.OnClickListener() {
+        Button buttMC6 = findViewById(R.id.buttonMC6);
+        buttMC6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openReview();
             }
         });
 
-        Button butt52 = findViewById(R.id.button52);
-        butt52.setOnClickListener(new View.OnClickListener() {
+        Button buttMC52 = findViewById(R.id.buttonMC52);
+        buttMC52.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openChipotleMenu();
+                openMcdonaldsMenu();
             }
         });
 
-        Button butt99 = findViewById(R.id.button99);
-        butt99.setOnClickListener(new View.OnClickListener() {
+        Button buttMC99 = findViewById(R.id.buttonMC99);
+        buttMC99.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openAddReview();
@@ -38,13 +38,13 @@ public class chipotle_page extends AppCompatActivity {
         });
     }
 
-    public void openChipotleMenu() {
-        Intent intent = new Intent(this, chipotle_menu.class);
+    public void openMcdonaldsMenu() {
+        Intent intent = new Intent(this, mcdonalds_menu.class);
         startActivity(intent);
     }
 
     public void openReview() {
-        Intent intent = new Intent(this, chipotle_review.class);
+        Intent intent = new Intent(this, mcdonalds_review.class);
         startActivity(intent);
     }
 
